5a943c54da06ef5a8d2215917e39126f9f7e45e0
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-04-04 01:43:03 -0700

    imported react icons and axios

--- github-api-project/src/App.jsx
+++ github-api-project/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Navbar from './componets/Navbar/Navbar';
+import Data_Container from './componets/Data_Container/Data_Container';
+import Api_Fetch from './componets/Api_Fetch/Api_Fetch';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-    heklo
+    <Navbar/>
+    <Data_Container/>
+    <Api_Fetch username='mubashir05-beep'/>
     </div>
   )
 }
--- github-api-project/src/componets/Api_Fetch/Api_Fetch.jsx
+++ github-api-project/src/componets/Api_Fetch/Api_Fetch.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+const Api_Fetch = ({ username }) => {
+    axios
+      .get(`https://api.github.com/users/${username}/repos`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((erroe) => console.log("error was found"));
+  return <div>
 
-const Api_Fetch = () => {
-  return (
-    <div>Api_Fetch</div>
-  )
-}
+  </div>;
+};
 
-export default Api_Fetch
+export default Api_Fetch;
