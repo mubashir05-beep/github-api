0e3841db692386e087a15b7b116f1bfe633ee328
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-04-04 12:44:15 -0700

    completed the card component design

--- github-api-project/src/componets/Api_Fetch/Api_Fetch.jsx
+++ github-api-project/src/componets/Api_Fetch/Api_Fetch.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import './styles.css'
+import { BsPeople } from "react-icons/bs";
 
+import { RiGitRepositoryLine } from "react-icons/ri";
+import "./styles.css";
 
 const Api_Fetch = ({ username }) => {
   const [data, setData] = useState("");
@@ -16,18 +18,28 @@ const Api_Fetch = ({ username }) => {
 
   return (
     <div className="contianer">
-      <img src={data.avatar_url} alt="" srcset="" />
+      <img className="profileImg" src={data.avatar_url} alt="" srcset="" />
       <div>
         <div className="leftContainer">
           <div className="top">
             <div className="name">{data.name}</div>
-            <div className="username">{data.login}</div>
+            <a
+              target="_blank"
+              className="username"
+              href={`https://github.com/${data.login}`}
+            >
+              @{data.login}
+            </a>
             <div className="location">{data.location}</div>
           </div>
+          <div className="public_repo"><RiGitRepositoryLine/> {data.public_repos}</div>
           <div className="bottom">
-            <div className="public_repo">{data.public_repos}</div>
-            <div className="follower">{data.followers}</div>
-            <div className="following">{data.following}</div>
+            <div className="follower">
+              <BsPeople />
+              {data.followers} followers
+            </div>
+            <span>.</span>
+            <div className="following">{data.following} following</div>
           </div>
         </div>
       </div>
