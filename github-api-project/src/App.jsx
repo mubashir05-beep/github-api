33d94bbbcf92f97848c6666b126bddd491dc6df1
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-04-04 05:12:29 -0700

    worked on the axios and integrated api

--- github-api-project/src/App.jsx
+++ github-api-project/src/App.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import Navbar from './componets/Navbar/Navbar';
-import Data_Container from './componets/Data_Container/Data_Container';
-import Api_Fetch from './componets/Api_Fetch/Api_Fetch';
-import './App.css'
+import React, { createContext} from "react";
+import Navbar from "./componets/Navbar/Navbar";
+import Data_Container from "./componets/Data_Container/Data_Container";
+import Api_Fetch from "./componets/Api_Fetch/Api_Fetch";
+import Header from './componets/Header/Header';
+import "./App.css";
 
+export const MainContext = createContext(null);
 function App() {
+
+
   return (
     <div className="App">
-    <Navbar/>
-    <Data_Container/>
-    <Api_Fetch username='mubashir05-beep'/>
+      <Navbar />
+      <Header/>
+      {/* <Header/> Will return username entered from user */}
+      {/* context will then forward the user name to api forward and thenm will make container there */}
+      <MainContext.Provider>
+      <Api_Fetch username="mubashir05-beep" />
+      </MainContext.Provider>
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- github-api-project/src/componets/Api_Fetch/Api_Fetch.jsx
+++ github-api-project/src/componets/Api_Fetch/Api_Fetch.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 const Api_Fetch = ({ username }) => {
+  const [data, setData] = useState("");
+  useEffect(() => {
     axios
-      .get(`https://api.github.com/users/${username}/repos`)
+      .get(`https://api.github.com/users/${username}`)
       .then((res) => {
-        console.log(res);
+        setData(res.data);
       })
-      .catch((erroe) => console.log("error was found"));
-  return <div>
+      .catch((error) => console.log(`Error ${error}`));
+  }, [username]);
 
-  </div>;
+  return (
+    <div className="contianer">
+      <img src={data.avatar_url} alt="" srcset="" />
+      <div>
+        <div className="leftContainer">
+          <div className="top">
+            <div className="name"></div>
+            <div className="username"></div>
+            <div className="location"></div>
+          </div>
+          <div className="bottom">
+            <div className="public_repo"></div>
+            <div className="follower"></div>
+            <div className="following"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Api_Fetch;
